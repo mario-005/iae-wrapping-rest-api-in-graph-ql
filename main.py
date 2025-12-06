eafb96a5daea857c243b32de9f67833196ac0761
mario-005 <fransisjamlean69@gmail.com>
2025-12-06 14:54:52 +0700

    update

--- main.py
+++ main.py
@@ -107,4 +107,4 @@ app.add_route("/graphql", graphql_app)
 
 if __name__ == "__main__":
     # Jalankan server
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
